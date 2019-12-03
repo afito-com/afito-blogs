b2d0dbcc7f4263b752d90263c553beb97dc117ec
redhair <thomasbonanni@afito.com>
2019-12-03 09:32:02 -0500

    build update

--- public/sw.js
+++ public/sw.js
@@ -27,20 +27,24 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-52164eda0b7a42710e23.js"
+    "url": "webpack-runtime-8df9aa8270fb8dfdbd5f.js"
   },
   {
     "url": "commons-1d4c3e9f086a07395290.js"
   },
   {
-    "url": "app-f10dcc1e60bb24ba0fb7.js"
+    "url": "app-e61a533d2b1b6f85f0a8.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-bb5433fc5c978771af2b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cc6d7441ee38e9267f3e8430ece806e7"
+    "revision": "b483ab6e8f7d1193b01facddde349ec6"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "d274adf0f008ef152ce70a312b04b730"
   },
   {
     "url": "manifest.webmanifest",
@@ -132,12 +136,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/blog`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-f10dcc1e60bb24ba0fb7.js`))) {
+  if (!resources || !(await caches.match(`/blog/app-e61a533d2b1b6f85f0a8.js`))) {
     return await fetch(event.request)
   }
 
@@ -150,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/blog/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
